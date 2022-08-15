092de03910f247d44e8c4439f53ba516e302600e
Swall0w <Swall0w@users.noreply.github.com>
2022-08-15 14:40:12 +0900

    [update]: replacing variable validation by argument parser required.

--- show_coco_errors.py
+++ show_coco_errors.py
@@ -6,30 +6,16 @@ from src import Evaluator, GtJson, ImagePrinter, PredJson
 
 def arg():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gt", "-g", type=str, default="")
-    parser.add_argument("--pred", "-p", type=str, default="")
-    parser.add_argument("--image_dir", "-d", type=str, default="")
+    parser.add_argument("--gt", "-g", type=str, default="", required=True)
+    parser.add_argument("--pred", "-p", type=str, default="", required=True)
+    parser.add_argument("--image_dir", "-d", type=str, default="", required=True)
     parser.add_argument("--iou_threshold", "-i", type=float, default=0.5)
     parser.add_argument("--confidence_threshold", "-c", type=float, default=0.7)
     parser.add_argument("--model_name", "-m", type=str, default="")
     parser.add_argument("--normalize", type=bool, default=False)
     args = parser.parse_args()
     print(args)
-    if len(args.gt) == 0:
-        print()
-        print("gt(coco json path) is required.")
-        print()
-        exit(0)
-    if len(args.pred) == 0:
-        print()
-        print("pred(coco json path) is required.")
-        print()
-        exit(0)
-    if len(args.image_dir) == 0:
-        print()
-        print("image_dir is required.")
-        print()
-        exit(0)
+
     return args
 
 
