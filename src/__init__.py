82b25601282c2163eaa8a0329a077ab1ebaaebe3
Swall0w <Swall0w@users.noreply.github.com>
2022-08-22 16:51:51 +0900

    [update]: init.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,9 @@
+__copyright__ = 'Copyright (C) 2022 Fast Accounting co.,ltd'
+__version__ = 'v0.0.0'
+__author__ = 'Fast Accounting co.,ltd'
+__url__ = 'https://github.com/FastAccounting/nobunaga'
+
+
 from .Constants import *
 from .Evaluator import *
 from .GtJson import *
